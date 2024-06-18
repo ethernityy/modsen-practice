5ddafd512c3313d5826096bfbfa0d34da7c02531
Евгений Подолицкий <yauhenipadalitski@gmail.com>
2024-06-18 16:28:51 +0300

    Modified navigation file, added functions: showResults and restartQuiz

--- quiz-app/navigation.js
+++ quiz-app/navigation.js
@@ -16,3 +16,22 @@ function prevQuestion() {
     showQuestion();
   }
 }
+
+function showResults() {
+  const quizContainer = document.querySelector('.quiz-container');
+  quizContainer.innerHTML = `
+  <div class='question-container'>
+  <h2 class='question-container'>Quiz completed<h2>
+  <p class='question'> You got ${correctAnswers} out of ${questions.length} points<p>
+  <button class='nav-btn' onclick='restartQuiz()'>Restart</button>
+  </div>
+  `;
+}
+
+function restartQuiz() {
+  currentQuestion = 0;
+  correctAnswers = 0;
+  showQuestion();
+}
+
+export { nextQuestion, prevQuestion, showResults, restartQuiz };

d33731661980a441400071302b0c3d9885e6f0e3
Евгений Подолицкий <yauhenipadalitski@gmail.com>
2024-06-18 16:00:51 +0300

    Created questions file

---
+++ quiz-app/questions.js
@@ -0,0 +1,85 @@
+const questions = [
+  {
+    question: 'How many planets are in the solar system?',
+    options: [
+      { text: '8', correct: true },
+      { text: '7', correct: false },
+      { text: '9', correct: false },
+      { text: '10', correct: false },
+    ],
+  },
+  {
+    question: 'What is the smallest ocean in the world?',
+    options: [
+      { text: 'Atlantic Ocean', correct: false },
+      { text: 'Indian Ocean', correct: false },
+      { text: 'Arctic Ocean', correct: true },
+      { text: 'Pacific Ocean', correct: false },
+    ],
+  },
+  {
+    question: 'What is the freezing point of water?',
+    options: [
+      { text: '0', correct: true },
+      { text: '-5', correct: false },
+      { text: '-6', correct: false },
+      { text: '5', correct: false },
+    ],
+  },
+  {
+    question: 'What is the longest river in the world?',
+    options: [
+      { text: 'Nile', correct: true },
+      { text: 'Amazon', correct: false },
+      { text: 'Yangtze', correct: false },
+      { text: 'Mississippi', correct: false },
+    ],
+  },
+  {
+    question: 'How many chromosomes are in the human genome?',
+    options: [
+      { text: '42', correct: false },
+      { text: '44', correct: false },
+      { text: '46', correct: true },
+      { text: '48', correct: false },
+    ],
+  },
+  {
+    question: 'Which of these characters are friends with Harry Potter?',
+    options: [
+      { text: 'Ron Weasley', correct: true },
+      { text: 'Draco Malfoy', correct: false },
+      { text: 'Hermione Granger', correct: true },
+      { text: 'Severus Snape', correct: false },
+    ],
+  },
+  {
+    question: 'What is the capital of Canada?',
+    options: [
+      { text: 'Toronto', correct: false },
+      { text: 'Ottawa', correct: true },
+      { text: 'Vancouver', correct: false },
+      { text: 'Montreal', correct: false },
+    ],
+  },
+  {
+    question: 'What is the Jewish New Year called?',
+    options: [
+      { text: 'Hanukkah', correct: false },
+      { text: 'Yom Kippur', correct: false },
+      { text: 'Kwanzaa', correct: false },
+      { text: 'Rosh Hashanah', correct: true },
+    ],
+  },
+  {
+    question: 'What is the capital of France?',
+    options: [
+      { text: 'New York', correct: false },
+      { text: 'London', correct: false },
+      { text: 'Paris', correct: true },
+      { text: 'Dublin', correct: false },
+    ],
+  },
+];
+
+export { questions };

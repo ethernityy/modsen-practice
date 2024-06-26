048673fcbc1ea3ac660d09106af2df9a3fe54e6d
Евгений Подолицкий <yauhenipadalitski@gmail.com>
2024-06-26 23:15:45 +0300

    feat: initialized server, postman requests via localhost passed successfully

---
+++ http/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+const apiKey = '6b5a66b34f4393cc19882d3e458b07bd';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/';
+
+app.get('/status', async (req, res) => {
+  try {
+    const city = req.query.city || 'Vitebsk';
+    const response = await axios.get(`${baseUrl}weather`, {
+      params: {
+        q: city,
+        appid: apiKey,
+      },
+    });
+    res.json({ status: response.status, data: response.data });
+  } catch (error) {
+    if (error.response) {
+      res.json({ status: error.response.status, data: error.response.data });
+    } else {
+      res.json({ status: 500, data: 'Internal Server Error' });
+    }
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
